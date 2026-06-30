7197ce57fe09ba61a3e069e48cd776576570a06c
Arya Dwi Putra <aryadptr.developer@gmail.com>
2026-06-30 08:08:56 +0700

    feat(seeder): auto-create super admin on fresh install

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::factory()->asSuperAdmin()->create([
+            'name' => 'Admin',
+            'email' => 'admin@taska.test',
+            'password' => Hash::make('password'),
+        ]);
+
         $this->call([
             TciPersonnelSeeder::class,
             TciProjectSeeder::class,
